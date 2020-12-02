2ce4488b36d634616cbc652156aef556946b0fbd
Santiago Fernandez <cenybar@gmail.com>
2020-12-02 18:02:18 +0100

    complete tictactoe ex

--- 5_Dictionaries/characterCount.py
+++ 5_Dictionaries/characterCount.py
@@ -1,5 +1,7 @@
 # characterCount.py
 
+import pprint
+
 message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
 count = {}
 
@@ -7,4 +9,4 @@ for character in message:
     count.setdefault(character, 0)
     count[character] += 1
 
-print(count)
+pprint.pprint(count)
