e34a2087031107cf0aafdd579b6f0cecb40fa0f1
Santiago Fernandez <cenybar@gmail.com>
2020-12-07 18:56:00 +0100

    add Ch6 bulletPointAdder ex

---
+++ 6_STRINGS/bulletPointAdder.py
@@ -0,0 +1,12 @@
+# bulletPointAdder.py
+
+import pyperclip
+text = pyperclip.paste()
+
+# Separate lines and add stars
+lines = text.split('\n')
+for i in range(len(lines)):
+    lines[i] = '* ' + lines[i]
+
+text = '\n'.join(lines)
+pyperclip.copy(text)
